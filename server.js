aa2e6c6d0800c3c2e80020ae0cb9dfb738019be9
Abir Hussain Khan Abid <abirkhanrcc@gmail.com>
2023-12-01 01:17:43 +0600

    connected server to database

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-app.listen(port,()=>{
-    console.log(`Server is listening on port ${port}`);
-})
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const mongo_uri = process.env.MONGO_URI;
+
+dotenv.config();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// connect database
+mongoose
+  .connect(mongo_uri)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(port, () => {
+  console.log(`Foodly backend is running on port ${port}`);
+});
