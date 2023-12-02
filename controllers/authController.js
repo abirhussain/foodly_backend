052ddc6f65e77fbb4c5218ed9c0134529da9ad0e
Abir Hussain Khan Abid <abirkhanrcc@gmail.com>
2023-12-02 22:34:38 +0600

    fixed login signup issues

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,10 +6,13 @@ const User = require("../models/User");
 
 module.exports = {
   createUser: async (req, res) => {
-    const { userName, email, password } = req.body;
+    const { username, email, password } = req.body;
+
     try {
-      const user = await admin.auth().getUserByEmail(email);
-      if (user.code === "auth/user-not-found") {
+      await admin.auth().getUserByEmail(email);
+      res.status(400).json({ message: "Email already exist" });
+    } catch (error) {
+      if (error.code === "auth/user-not-found") {
         try {
           const userResponse = await admin.auth().createUser({
             email: email,
@@ -17,10 +20,9 @@ module.exports = {
             emailVerified: false,
             disabled: false,
           });
-          console.log(userResponse.uid);
 
           const newUser = new User({
-            username: userName,
+            userName: username,
             email: email,
             password: CryptoJS.AES.encrypt(
               password,
@@ -29,50 +31,59 @@ module.exports = {
             uid: userResponse.uid,
             userType: "Client",
           });
+
           await newUser.save();
           res.status(201).json({ status: true });
         } catch (error) {
-          res.status(500).json({ status: false, error: "Error creating user" });
+          res.status(500).json({
+            status: false,
+            error: "Error creating user",
+          });
         }
       } else {
-        res.status(400).json({ message: "Email already exist" });
+        res.status(422).json({
+          message: "missing information",
+        });
       }
-    } catch (error) {
-      res.status(500).json({
-        message: "Something went wrong. Please try again after sometimes",
-      });
     }
   },
+
   loginUser: async (req, res) => {
-    const { email } = req.body;
     try {
       const user = await User.findOne(
-        { email: email },
+        { email: req.body.email },
         { __v: 0, updatedAt: 0, createdAt: 0 }
       );
-      !user && res.status(401).json({ message: "Wrong credentials" });
-      const decryptedPassword = CryptoJS.AES.decrypt(
-        password,
-        process.env.SECRET
-      );
-      const decrypted = decryptedPassword.toString(CryptoJS.enc.Utf8);
-      decrypted !== password &&
-        res.status(401).json({ message: "Wrong credentials" });
-      const userToken = jwt.sign(
-        {
-          id: user._id,
-          userType: user.userType,
-          email: user.email,
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: "21d" }
-      );
 
-      const { password, email, ...others } = user._doc;
+      if (user) {
+        const decryptedpassword = CryptoJS.AES.decrypt(
+          user.password,
+          process.env.SECRET
+        );
 
-      res.status(200).json({ ...others, userToken });
+        if (
+          decryptedpassword.toString(CryptoJS.enc.Utf8) != req.body.password
+        ) {
+          res.status(401).json("Wrong password");
+        } else {
+          const userToken = jwt.sign(
+            {
+              id: user._id,
+              userType: user.userType,
+              email: user.email,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "21d" }
+          );
+
+          const { password, email, ...others } = await user._doc;
+          res.status(200).json({ ...others, userToken });
+        }
+      } else {
+        res.status(401).json("Wrong credentials");
+      }
     } catch (error) {
-      res.status(500).json({ status: false, error: error.message });
+      res.status(500).json({ status: false, err: error.message });
     }
   },
 };
